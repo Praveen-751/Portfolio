750ebd1b11c0ad37ce129ecf99da3ebc667d9cb6
Praveen <chinnamsettipraveenkumar@gmail.com>
2025-03-23 22:35:35 -0400

    udoated the env for prod and dev

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio",
 };
 
 module.exports = nextConfig;
