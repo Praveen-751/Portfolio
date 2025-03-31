1ca8ea1e68bacc8f3661df364f0444b0fda4969e
Praveen <chinnamsettipraveenkumar@gmail.com>
2025-03-30 20:56:18 -0400

    updated portfolio

--- components/projects.jsx
+++ components/projects.jsx
@@ -3,10 +3,16 @@ import "./projects.css"; // Add a CSS file for styling
 
 const Projects = () => {
     return (
-        <div className="projects-main">
-            <p>This is @ contact page</p>
-        </div>
-    )
+      <div className="projects-main">
+        <p>This is @ Projects page</p>
+        <p>Linkedin</p>
+        <p>Github</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>Mail</p>
+      </div>
+    );
 }
 
 export default Projects;
