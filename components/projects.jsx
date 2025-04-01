a8b5d5549751654bfe211d67d6aede14d0ac0e0f
Praveen <chinnamsettipraveenkumar@gmail.com>
2025-03-31 23:23:11 -0400

    started projects section

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./projects.css"; // Add a CSS file for styling
+import Cards from "./cards";
 
 const Projects = () => {
-    return (
-      <div className="projects-main">
-        <p>This is @ Projects page</p>
-        <p>Linkedin</p>
-        <p>Github</p>
-        <p>Twitter</p>
-        <p>Instagram</p>
-        <p>Facebook</p>
-        <p>Mail</p>
+  return (
+    <div className="projects-main">
+      <h1 className="projects-heading"> My Recent Projects Including Full Stack and Machine Learning</h1>
+      <div className="cards-section">
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Projects;
