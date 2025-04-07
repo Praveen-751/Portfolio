9f20796c19eec02ce8be1f272d753425d4f9772b
Praveen <chinnamsettipraveenkumar@gmail.com>
2025-04-07 01:14:24 -0400

    projects for desktop done

--- components/cards.jsx
+++ components/cards.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./cards.css"; // Add a CSS file for styling
 
-const Cards = ({ props, text }) => {
+const Cards = ({ props, text, id }) => {
+  
+    const backgroundColors = {
+      1: "#0a469a",
+      2: "#d76235",
+      3: "#24405f",
+      4: "2px solid #FF6B6B",
+      5: "2px solid #228B22",
+      6: "2px solid #1D4ED8",
+    };
+
   return (
     <div className="cards-main">
       <div className="cards-content">
         <img className="cards-image" src={props} alt="svg icon" />
-        <div className="cards-text">
+        <div
+          className="cards-text"
+          style={{
+            backgroundColor: backgroundColors[id] || "rgba(0, 0, 0, 0.5)",
+          }}
+        >
           {text}
-          <button className="cards-button"> Check out !</button>
+          <button className="cards-button"
+            style={{
+            border: backgroundColors[id + 3] || "rgba(0, 0, 0, 0.5)",
+          }}
+          > Check out !</button>
         </div>
       </div>
     </div>
--- components/projects.jsx
+++ components/projects.jsx
@@ -5,12 +5,33 @@ import Cards from "./cards";
 const Projects = () => {
   return (
     <div className="projects-main">
-      <h1 className="projects-heading"> My Recent Projects Including Full Stack and Machine Learning</h1>
+      <h1 className="projects-heading">
+        {" "}
+        My Recent Projects Including Full Stack and Machine Learning
+      </h1>
       <div className="cards-section">
-              <Cards props={'Shot_1.jpeg'} text={"Skin Detection System"} />
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards
+          props={"Skin_Diesease.png"}
+          text={
+            "Image-based system for detecting skin diseases using processing algorithms, aiding diagnosis in remote areas with limited dermatologist access."
+          }
+          id={1}
+        />
+
+        <Cards
+          props={"crop_price_prediction.png"}
+          text={
+            "Machine learning predicts vegetable prices using weather data, helping farmers plan crops and maximize profits through accurate forecasting."
+          }
+          id={2}
+        />
+        <Cards
+          props={"netwrok_anomaly.png"}
+          text={
+            "Machine learning-based system to detect network anomalies like DDoS and unauthorized access, improving security over traditional methods."
+          }
+          id={3}
+        />
       </div>
     </div>
   );
